4d3dc2519153c9164bf8373626b9fec49b116c36
Patrick Roy <ravenlost2@gmail.com>
2020-11-21 21:08:32 +0100

    Changed license and fixed a function arg 's default value when null

--- src/Locale.js
+++ src/Locale.js
@@ -1,12 +1,27 @@
 /**
  * Locale class wrapper to get translations data from a JSON array object
  * 
- * Copyright (C) 2020, Patrick Roy
- * This file may be used under the terms of the GNU Lesser General Public License, version 3.
- * For more details see: https://www.gnu.org/licenses/lgpl-3.0.html
- * This program is distributed in the hope that it will be useful - WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
+ * MIT License
+ * 
+ * Copyright (c) 2020 Patrick Roy
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * @requires sprintf() function:
  *   https://github.com/alexei/sprintf.js,
@@ -248,7 +263,12 @@ CorbeauPerdu.i18n.Locale = function (lang, domain, data, useCustomPluralForms, d
    * @param boolean $replaceLineBreaks replace '\n' with '<br/>'
    * @return string
    */
-  var _stringToWeb = function (value, replaceLineBreaks = true) {
+  var _stringToWeb = function (value, replaceLineBreaks) {
+    
+    if( replaceLineBreaks === undefined ){
+      replaceLineBreaks = true;
+	}
+    
 	var retval = String(value).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
 	if(replaceLineBreaks == true) {
       retval = String(value).replace(/\n/g, '<br/>');
